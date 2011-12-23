26b1ae65e4a5db4af82f1bc4fbe058db58d58ac0
Denis Zhdanov <denis.zhdanov@gmail.com>
2011-12-23 19:12:05 +0200

    Quoted text export fix

--- PlusFeed.pm
+++ PlusFeed.pm
@@ -25,9 +25,7 @@ sub _initialize {
     my $hash = {@_};
     croak "Please provide Google+ API Key" if (not $hash->{key});
     $self->{_key} = $hash->{key};
-    my $plus = Google::Plus->new(
-        key => $hash->{key}
-    );
+    my $plus = Google::Plus->new(key => $hash->{key});
     $self->{_plus_object} = $plus;
     croak "Please provide Google+ User Id" if (not $hash->{user});
     $self->{_user} = $hash->{user};
@@ -37,13 +35,13 @@ sub _initialize {
     # page only consists 20 items for now
     # Google::Plus not supporting maxItems yet
     $self->{_pages} = $hash->{pages};
-    $self->{_pages} //= 0; # default is 0 means all pages
-    
+    $self->{_pages} //= 0;    # default is 0 means all pages
+
     # caching
     # cache - must be already initialized CHI object
     $self->{_cache_object} = $hash->{cache};
-    $self->{_cache_ttl} = $hash->{cache_ttl};
-    $self->{_cache_ttl} //= 300; # default seconds
+    $self->{_cache_ttl}    = $hash->{cache_ttl};
+    $self->{_cache_ttl} //= 300;    # default seconds
 
     return 1;
 }
@@ -55,9 +53,15 @@ sub get_api_key {
 
 sub set_api_key {
     my $self = shift;
-    my $key = shift;
+    my $key  = shift;
     $self->{_key} = $key;
-    $self->_initialize(key => $self->{_key}, user => $self->{_user}, pages => $self->{_pages}, cache => $self->{_cache}, cache_ttl => $self->{_cache_ttl});
+    $self->_initialize(
+        key       => $self->{_key},
+        user      => $self->{_user},
+        pages     => $self->{_pages},
+        cache     => $self->{_cache},
+        cache_ttl => $self->{_cache_ttl}
+    );
 }
 
 sub get_user_id {
@@ -69,7 +73,13 @@ sub set_user_id {
     my $self = shift;
     my $user = shift;
     $self->{_user} = $user;
-    $self->_initialize(key => $self->{_key}, user => $self->{_user}, pages => $self->{_pages}, cache => $self->{_cache}, cache_ttl => $self->{_cache_ttl});
+    $self->_initialize(
+        key       => $self->{_key},
+        user      => $self->{_user},
+        pages     => $self->{_pages},
+        cache     => $self->{_cache},
+        cache_ttl => $self->{_cache_ttl}
+    );
 }
 
 sub get_plus_object {
@@ -98,18 +108,18 @@ sub get_cache_object {
 }
 
 sub get_rss_stream {
-    my $self   = shift;
-    my $hash   = {@_};
-    my $plus   = $self->{_plus_object};
-    my $rss    = $self->{_rss_object};
-    my $pages  = $self->{_pages};
-    my $user   = $self->{_user};
-    my $cache  = $self->{_cache_object};
-    
+    my $self  = shift;
+    my $hash  = {@_};
+    my $plus  = $self->{_plus_object};
+    my $rss   = $self->{_rss_object};
+    my $pages = $self->{_pages};
+    my $user  = $self->{_user};
+    my $cache = $self->{_cache_object};
+
     my $rss_stream;
     if (defined $cache) {
-       $rss_stream  = $cache->get( 'RSS'.$user );
-       return $rss_stream if defined $rss_stream;
+        $rss_stream = $cache->get('RSS' . $user);
+        return $rss_stream if defined $rss_stream;
     }
     my $person = $plus->person($user);
     $self->{_person_object} = $person;
@@ -119,13 +129,14 @@ sub get_rss_stream {
         link        => "https://plus.google.com/$user/posts",
         description => "Google+ RSS feed for $person_name"
     );
+
     # get this person's activities
     my $activities = $plus->activities($user, 'public');
     $self->{_activities_object} = $activities;
     while ($activities->{nextPageToken}) {
         my $next = $activities->{nextPageToken};
         for my $item (@{$activities->{items}}) {
-    	    no warnings;
+            no warnings;
             my $object = $item->{object};
             my $body   = $item->{annotation};
             if ($body) { $body = $body . "<br />"; }
@@ -140,11 +151,8 @@ sub get_rss_stream {
                         $body = $body . $att->{image}->{url} . '"></a>';
                     }
                     elsif ($att->{objectType} eq 'video') {
-                        $body = 
-                            $body
-                          . '<a href="'
-                          . $att->{url}
-                          . '"><img src="';
+                        $body =
+                          $body . '<a href="' . $att->{url} . '"><img src="';
                         $body = $body . $att->{image}->{url} . '"></a>';
                     }
                     else {
@@ -157,7 +165,8 @@ sub get_rss_stream {
                 }
             }
             if ($body) { $body = $body . "<br />"; }
-            $body = $body . $item->{content};
+            $body = $body . '<a href="' . $item->{url} . '"';
+            $body = $body . $item->{content} . '"></a>';
             $rss->add_item(
                 title       => $item->{title},
                 description => $body,
@@ -171,11 +180,14 @@ sub get_rss_stream {
           if defined $next && $self->{_pages} >= 0;
         --$self->{_pages} if $self->{_pages} > 0;
     }
+
     # put string in cache
-    $cache->set( 'RSS'.$user, $rss->as_string, $self->{_cache_ttl} ) if defined $cache;
+    $cache->set('RSS' . $user, $rss->as_string, $self->{_cache_ttl})
+      if defined $cache;
+
     # renew objects
     $self->{_plus_object} = $plus;
-    $self->{_rss_object} = $rss;
+    $self->{_rss_object}  = $rss;
     return $rss->as_string;
 
 }

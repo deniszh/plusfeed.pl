9b152b9f728be88fb6e1a1f7bfd8fbadb1bcdc31
Denis Zhdanov <denis.zhdanov@gmail.com>
2011-12-24 18:44:22 +0200

    Code was refactored a bit

--- PlusFeed.pm
+++ PlusFeed.pm
@@ -6,11 +6,10 @@ use Carp;
 
 use Google::Plus;
 require 5.10.1;
-use v5.10.1;
 use XML::RSS;
 use utf8;
 
-our $VERSION = '0.01';
+our $VERSION = '0.03';
 
 sub new {
     my $class = shift;
@@ -45,6 +44,127 @@ sub _initialize {
 
     return 1;
 }
+
+
+# make body of RSS entry using G+ activity item
+# many raw html magic :)
+sub _make_body {
+    my $self = shift;
+    my $item = shift;
+
+    no warnings;
+    my $object = $item->{object};
+    my $body   = $item->{annotation};
+    if ($body) { $body = $body . "<br />"; }
+    if ($object->{attachments}) {
+        for my $att (@{$object->{attachments}}) {
+            if ($att->{objectType} eq 'photo') {
+                $body =
+                    $body
+                  . '<a href="'
+                  . $att->{fullImage}->{url}
+                  . '"><img src="';
+                $body = $body . $att->{image}->{url} . '"></a>';
+            }
+            elsif ($att->{objectType} eq 'video') {
+                $body = $body . '<a href="' . $att->{url} . '"><img src="';
+                $body = $body . $att->{image}->{url} . '"></a>';
+            }
+            else {
+                $body =
+                    $body
+                  . ' <a href="'
+                  . $att->{url} . '">'
+                  . $att->{displayName} . '</a>';
+            }
+        }
+    }
+    if ($body) { $body = $body . "<br />"; }
+    $body = $body . '<a href="' . $object->{url} . '"';
+    $body = $body . $object->{content} . '"></a>';
+
+    return $body;
+}
+
+### put string in cache
+sub _cache_put {
+    my $self      = shift;
+    my $string    = shift;
+    my $namespace = shift;
+    my $key       = shift;
+
+    my $cache = $self->{_cache_object};
+    return if not defined $cache;
+
+    # put string in cache
+    $cache->set($namespace . $key, $string, $self->{_cache_ttl});
+}
+
+### get tring from cache
+sub _cache_get {
+    my $self      = shift;
+    my $namespace = shift;
+    my $key       = shift;
+
+    my $cache = $self->{_cache_object};
+    return if not defined $cache;
+
+    return $cache->get($namespace . $key);
+}
+
+sub get_rss_stream {
+    my $self  = shift;
+    my $hash  = {@_};
+    my $plus  = $self->{_plus_object};
+    my $rss   = $self->{_rss_object};
+    my $pages = $self->{_pages};
+    my $user  = $self->{_user};
+
+    my $rss_stream = $self->_cache_get('RSS', $user);
+    return $rss_stream if defined $rss_stream;
+
+    my $person = $plus->person($user);
+    $self->{_person_object} = $person;
+    my $person_name = $person->{displayName};
+    $rss->channel(
+        title       => "Google+ for $person_name",
+        link        => "https://plus.google.com/$user/posts",
+        description => "Google+ RSS feed for $person_name"
+    );
+
+    # get this person's activities
+    my $activities = $plus->activities($user, 'public');
+    $self->{_activities_object} = $activities;
+    while ($activities->{nextPageToken}) {
+        my $next = $activities->{nextPageToken};
+        for my $item (@{$activities->{items}}) {
+            my $body = $self->_make_body($item);
+            $rss->add_item(
+                title       => $item->{title},
+                description => $body,
+                guid        => $item->{etag},
+                url         => $item->{url},
+                author      => $item->{actor}->{displayName},
+                date        => $item->{published},
+            );
+        }
+        $activities = $plus->activities($user, 'public', $next)
+          if defined $next && $self->{_pages} >= 0;
+        --$self->{_pages} if $self->{_pages} > 0;
+    }
+
+    # put string in cache
+    $self->_cache_put('RSS', $user, $rss->as_string);
+
+    # renew objects
+    $self->{_plus_object} = $plus;
+    $self->{_rss_object}  = $rss;
+    return $rss->as_string;
+
+}
+
+### accessors below
+### was added for clarity :)
 
 sub get_api_key {
     my $self = shift;
@@ -107,89 +227,5 @@ sub get_cache_object {
     return $self->{_cache_object};
 }
 
-sub get_rss_stream {
-    my $self  = shift;
-    my $hash  = {@_};
-    my $plus  = $self->{_plus_object};
-    my $rss   = $self->{_rss_object};
-    my $pages = $self->{_pages};
-    my $user  = $self->{_user};
-    my $cache = $self->{_cache_object};
-
-    my $rss_stream;
-    if (defined $cache) {
-        $rss_stream = $cache->get('RSS' . $user);
-        return $rss_stream if defined $rss_stream;
-    }
-    my $person = $plus->person($user);
-    $self->{_person_object} = $person;
-    my $person_name = $person->{displayName};
-    $rss->channel(
-        title       => "Google+ for $person_name",
-        link        => "https://plus.google.com/$user/posts",
-        description => "Google+ RSS feed for $person_name"
-    );
-
-    # get this person's activities
-    my $activities = $plus->activities($user, 'public');
-    $self->{_activities_object} = $activities;
-    while ($activities->{nextPageToken}) {
-        my $next = $activities->{nextPageToken};
-        for my $item (@{$activities->{items}}) {
-            no warnings;
-            my $object = $item->{object};
-            my $body   = $item->{annotation};
-            if ($body) { $body = $body . "<br />"; }
-            if ($object->{attachments}) {
-                for my $att (@{$object->{attachments}}) {
-                    if ($att->{objectType} eq 'photo') {
-                        $body =
-                            $body
-                          . '<a href="'
-                          . $att->{fullImage}->{url}
-                          . '"><img src="';
-                        $body = $body . $att->{image}->{url} . '"></a>';
-                    }
-                    elsif ($att->{objectType} eq 'video') {
-                        $body =
-                          $body . '<a href="' . $att->{url} . '"><img src="';
-                        $body = $body . $att->{image}->{url} . '"></a>';
-                    }
-                    else {
-                        $body =
-                            $body
-                          . ' <a href="'
-                          . $att->{url} . '">'
-                          . $att->{displayName} . '</a>';
-                    }
-                }
-            }
-            if ($body) { $body = $body . "<br />"; }
-            $body = $body . '<a href="' . $item->{url} . '"';
-            $body = $body . $item->{content} . '"></a>';
-            $rss->add_item(
-                title       => $item->{title},
-                description => $body,
-                guid        => $item->{etag},
-                url         => $item->{url},
-                author      => $item->{actor}->{displayName},
-                date        => $item->{published},
-            );
-        }
-        $activities = $plus->activities($user, 'public', $next)
-          if defined $next && $self->{_pages} >= 0;
-        --$self->{_pages} if $self->{_pages} > 0;
-    }
-
-    # put string in cache
-    $cache->set('RSS' . $user, $rss->as_string, $self->{_cache_ttl})
-      if defined $cache;
-
-    # renew objects
-    $self->{_plus_object} = $plus;
-    $self->{_rss_object}  = $rss;
-    return $rss->as_string;
-
-}
-
 1;
+
